b54a1786ada6539fbda05852b961ad35378b3ac9
Joel Maupin <jmaupin82@gmail.com>
2014-02-25 10:59:18 -0600

    added crisper file.

---
+++ webservice/crisper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import urllib2
+
+#Awesome crisper made by Joel the badass
+print "about to call url"
+response = urllib2.urlopen('http://www.mesonet.org/data/public/mesonet/current/current.csv.txt')
+print "waiting for response"
+body = response.read()
+print "Print out each line of the file"
+#break the data into lines
+
+lines = body.split("\n")
+formatedLines = ""
+for line in lines:
+	data = line.split(",")
+	#get data out of the Lat Long and Temp locations
+	lat = data[3]
+	lon = data[4]
+
+print "done printing"
+
