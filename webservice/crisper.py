743618f5e86f733ee7c86df42553943ae08f69eb
Grant Bacon <btnarg@gmail.com>
2014-03-06 11:20:14 -0600

    [Mez] Processing data into tuple sets is now done with Mez

--- webservice/crisper.py
+++ webservice/crisper.py
@@ -3,12 +3,13 @@ from urllib2 import urlopen
 from time import time, sleep
 import threading
 import os
+import mez
 
 class CrisperTimer(threading.Thread):
     def run(self):
         return
 
-class Crisper(threading.Thread):
+class Crisper:
     """
     Crisper - "Keeps Data Fresh"™
 
@@ -22,11 +23,14 @@ class Crisper(threading.Thread):
     def __init__(self, stor_dir = ".", latest_file_time = 0):
         self.latest_file_time = latest_file_time
         self.stor_dir = stor_dir
+        self.meso = mez.Mez()
         # TODO: call timer to repeatedly run function
-#        self.test_time()
+        # self.test_time()
+        return
 
     def test_time(self):
-
+        result = self.meso.get_data()
+        print result
 
     def store_data(self, svg):
         return
