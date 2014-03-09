389ac4c89718976602a2ab87493ec584f5af82e1
Grant Bacon <btnarg@gmail.com>
2014-03-09 13:09:43 -0500

    Web: crisper writes to files, the /latest/timestamp and /latest/image endpoints are functional.

--- webservice/crisper.py
+++ webservice/crisper.py
@@ -1,10 +1,9 @@
 # -*- coding: utf8 -*-
 from urllib2 import urlopen
 from time import time, sleep
-import threading
-from subprocess import Popen
-
+from subprocess import Popen, PIPE
 from os.path import dirname, abspath, join
+import threading
 import mez
 
 def relative_path(suffix):
@@ -29,7 +28,7 @@ class Crisper(threading.Thread):
         stor_dir string Location to store saved SVG files
     """
 
-    def __init__(self, stor_dir = ".", latest_file_time = 0, timeout = 300):
+    def __init__(self, stor_dir = ".", latest_file_time = 0, timeout = 300, results = None):
         threading.Thread.__init__(self)
 
         self.daemon = True
@@ -37,35 +36,46 @@ class Crisper(threading.Thread):
         self.latest_file_time = latest_file_time
         self.stor_dir = stor_dir
         self.timeout = timeout
+        self.results = results
 
         self.meso = mez.Mez()
-        self.halt_event = threading.Event()
 
-    # override Thread.join()
-    def join(self, timeout=None):
-        self.halt_event.set()
-        threading.Thread.join(self, timeout)
-
-    # called when thread is executed
     def run(self):
-        while not self.halt_event.is_set():
-            try:
-                svg_data = self._generate_latest()
-                self._store_data(svg_data)
-            except IOError, i:
-                print "[Error]: Unable to write to file. Check permissions of directory."
-                raise
-            except Exception, e:
-                raise CrisperException("Failed in normal thread exceution", e)
+        while True:
+	        try:
+	            self._generate_latest()
+	        except Exception, e:
+                    print e
+	            raise CrisperException("Failed in normal thread exceution", e)
         sleep(self.timeout)
 
     def _generate_latest(self):
-        print "Generating latest..."
         data = self.meso.get_data()
-        print "Should call a subprocess and generate an image...~"
-        sleep(20)
+
+        try:
+            acl2 = Popen(relative_path("test.py"), stdout=PIPE, stdin=PIPE)
+            result = acl2.communicate(input = data)[0]
+        except:
+            print "Error opening subprocess"
+
+        file_timestamp = self._store_data(result)
+        if file_timestamp:
+            self.latest_file_time = file_timestamp
+
+        sleep(self.timeout)
 
     def _store_data(self, svg):
-        pass
+        now = str(int(time()))
 
+        try:
+            new_filename = relative_path(self.stor_dir) + "/" + now + '.svg'
+            file = open(new_filename, 'w+')
+            file.write(svg)
+            file.close()
+            self.latest_file_name = new_filename
+        except IOError, i:
+            print "Could not create file @" + now
+            print i
 
+        print "Created new file: " + now + ".svg"
+        return now
--- webservice/web_ui.py
+++ webservice/web_ui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-from bottle import run, route, request, template, static_file
+from bottle import run, route, request, template, static_file, response
 from os.path import dirname, abspath, join
 import subprocess, Queue
 from crisper import Crisper
@@ -26,11 +26,28 @@ def about():
 
 @route('/latest/timestamp')
 def latest_timestamp():
-    pass
+    return crisp.latest_file_time
+
+@route('/latest/image')
+def latest_image():
+    try:
+        file = open(crisp.latest_file_name, 'r')
+        image_data = file.read()
+        file.close()
+    except IOError, i:
+        print "[ERROR]: " + i
+        response.status = 500
+        response.body = 'Error opening latest image'
+        return
+
+    response.set_header('Content-Type', 'image/svg+xml')
+    response.body = image_data
+    return image_data
 
 try:
-    crisp = Crisper()
+    crisp = Crisper(stor_dir = "svg_crisper", timeout = 300)
     crisp.start()
     run(host=ADDR, port=PORT)
-except KeyboardInterrupt:
-    crisp.join()
+except Exception, e:
+    print e
+
