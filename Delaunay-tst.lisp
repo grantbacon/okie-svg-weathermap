05e39354aceeb07358f84307609ceb848928bb34
Robert Wenrich <rwenrich@ou.edu>
2014-04-10 03:04:09 -0500

    Updated Delaunay check-expects and affected functions

--- Delaunay-tst.lisp
+++ Delaunay-tst.lisp
@@ -27,3 +27,93 @@
  (let* ((pointa (point 1 1 0))
         (pointb (point 1 1 0)))
        (distanceSQ pointa pointb)) 0)
+
+;tests for distance SQ
+(check-expect (distanceSQ (point 0 0 nil) (point 5 5 nil)) 50)
+(check-expect (distanceSQ (point nil 0 nil) (point 5 5 nil)) nil)
+(check-expect (distanceSQ nil (point 5 5 nil)) nil)
+(check-expect (distanceSQ (point 0 -5 nil) (point 5 5 nil)) 125)
+
+;tests for crossProduct
+(check-expect (equal (crossProduct (point 0 0 nil) (point 0 5 nil) (point 1 1 nil))
+                     0) nil)
+(check-expect (crossProduct (point 0 0 nil) (point 0  5 nil) (point 0 1 nil))
+              0)
+(check-expect (crossProduct (point nil 0 nil) (point 0 5 nil) (point 1 1 nil)) nil)
+(check-expect (crossProduct nil (point 0 5 nil) (point 1 1 nil)) nil)
+                     
+
+;tests for circumCircle
+(check-expect (circumCircle nil (point 0 0 nil) 
+                            (point 0 6 nil) (point 3 3 nil))
+              (circle (point 0 3 nil) 9))
+(check-expect (circumCircle nil (point nil 0 nil) 
+                            (point 0 6 nil) (point 3 3 nil)) nil)
+(check-expect (circumCircle nil nil (point 0 6 nil) (point 3 3 nil)) nil)
+
+
+;tests for updateLeftFace
+(check-expect (updateLeftFace (vEdge 3 15 nil 0) 3 1) (vEdge 3 15 1 0))
+(check-expect (updateLeftFace (vEdge 3 15 0 0) 3 1) nil)
+(check-expect (updateLeftFace (vEdge 3 15 nil 0) nil 1) nil)
+(check-expect (updateLeftFace nil 3 1) nil)
+
+;tests for findEdge
+(check-expect (findEdge 1 3 (avl-insert (empty-tree) 1 (vEdge 1 3 nil nil)) 1) 1)
+(check-expect (findEdge 5 3 (avl-insert (empty-tree) 1 (vEdge 1 3 nil nil)) 1) nil)
+(check-expect (findEdge 1 3 (avl-insert (empty-tree) 1 (vEdge 1 nil nil nil)) 1) nil)
+(check-expect (findEdge 1 3 (empty-tree) 1) nil)
+
+;tests for updateEdges
+
+
+;tests for findPoint
+(defun samplePoints ()
+   (avl-insert (avl-insert (avl-insert (avl-insert (empty-tree) 
+                                                   0 (point 1 4 nil))
+                                       1 (point 0 3 nil)) 
+                           2 (point 0 3 nil)) 
+               3 (point 1 2 nil)))              
+(check-expect (findPoint 0 0 2 (samplePoints) 4) 3)
+(check-expect (findPoint 0 1 2 (samplePoints) 4) 5)
+(check-expect (findPoint 0 0 -2 (samplePoints) 4) nil)
+(check-expect (findPoint 0 0 2 (empty-tree) 4) nil)
+ 
+
+;tests for bestPoint
+
+
+;tests for completeFacet
+
+
+;tests for triangulate
+
+
+;tests for closest-help
+
+
+;tests for findClosestNeighors
+
+
+;tests for conatins
+
+
+;tests for prepData
+
+
+;tests for sameSide
+
+
+;tests for pointInTriangle
+
+
+;tests for process-help
+
+
+;tests for processEdges
+
+
+;tests for genTriangles
+
+
+;tests for delstart
--- structures.lisp
+++ structures.lisp
@@ -1,5 +1,7 @@
 (in-package "ACL2")
 (include-book "data-structures/structures" :dir :system)
 
-(defstructure point x y color (:options :slot-writers))
-(defstructure triangle p1 p2 p3)
+(defstructure point (x (:assert (rationalp x))) (y (:assert (rationalp y))) 
+   color (:options (:assert (and (rationalp x) (rationalp y))):slot-writers))
+(defstructure triangle p1 p2 p3 
+   (:options (:assert (and (point-p p1) (point-p p2) (point-p p3)))))
