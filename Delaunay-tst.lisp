833f4f97e4b62bbd4ecca205c6b1ca2d0941e5ff
Robert Wenrich <rwenrich@ou.edu>
2014-04-14 01:02:51 -0500

    Added more tests, adjustments, and split lines to fit 80 char limit.

--- Delaunay-tst.lisp
+++ Delaunay-tst.lisp
@@ -3,8 +3,45 @@
 
 (include-book "Delaunay")
 
-(defun pointp (p)
-   (not (and (null (point-x p)) (null (point-y p)))))
+;sample data of points and correct edges to simplify testing
+(defun samplePoints ()
+   (let* ((p0 (avl-insert (empty-tree) 0 (point 0 0 nil)))
+          (p1 (avl-insert p0 1 (point 5 5 nil)))
+          (p2 (avl-insert p1 2 (point 0 3 nil)))
+          (p3 (avl-insert p2 3 (point 5 0 nil)))
+          (p4 (avl-insert p3 4 (point -2 -2 nil)))
+          (p5 (avl-insert p4 5 (point 9/2 3 nil)))
+          (p6 (avl-insert p5 6 (point 2 7 nil)))
+          (p7 (avl-insert p6 7 (point 0 10 nil)))
+          (p8 (avl-insert p7 8 (point 5 10 nil)))
+          (p9 (avl-insert p8 9 (point -5 8 nil))))
+         p9))
+
+(defun sampleEdges ()
+   (avl-insert
+        (list 5 7 (vEdge 2 6 1 0)
+           (list 3 3 (vEdge 3 5 1 0)
+              (list 2 1 (vEdge 1 6 0 1)
+                 (list 1 0 (vEdge 1 5 0 0) NIL NIL)
+                 (list 1 2 (vEdge 5 6 1 0) NIL NIL))
+              (list 2 5 (vEdge 1 8 0 1)
+                 (list 1 4 (vEdge 1 3 1 0) NIL NIL)
+                 (list 1 6 (vEdge 6 8 1 0) NIL NIL)))
+           (list 4 15 (vEdge 0 2 1 1)
+              (list 3 11 (vEdge 7 8 1 0)
+                 (list 2 9 (vEdge 0 5 1 1)
+                    (list 1 8 (vEdge 2 5 0 1) NIL NIL)
+                    (list 1 10 (vEdge 0 3 0 1) NIL NIL))
+                 (list 2 13 (vEdge 6 9 1 1)
+                    (list 1 12 (vEdge 6 7 1 0) NIL NIL)
+                    (list 1 14 (vEdge 2 9 1 0) NIL NIL)))
+              (list 3 17 (vEdge 3 4 1 0)
+                 (list 1 16 (vEdge 0 4 0 1) NIL NIL)
+                 (list 2 18 (vEdge 7 9 0 1)))))
+               19 (vEdge 0 9 1 0)))        
+
+(include-book "arithmetic/top" :dir :system)
+
 
 #| distanceSQ |#
 ;; Theorems
@@ -12,8 +49,9 @@
 ; Given x^2 > 0 for all x (where x is a real number)
 ; this implies that distance^2 should also be > 0
 (defthm distanceSQ-is-positive
-   (implies (and (pointp p1) (pointp p2))
-            (> (distanceSQ p1 p2) 0)))
+   (implies (and (point-p p1) (point-p p2))
+            (and (>= (distanceSQ p1 p2) 0)
+                 (rationalp (distanceSQ p1 p2)))))
 
 ;; Check Expects
 ; The distance squared of (1, 1) --> (2, 2) == 2
@@ -28,18 +66,23 @@
         (pointb (point 1 1 0)))
        (distanceSQ pointa pointb)) 0)
 
-;tests for distance SQ
+;tests for distanceSQ
 (check-expect (distanceSQ (point 0 0 nil) (point 5 5 nil)) 50)
 (check-expect (distanceSQ (point nil 0 nil) (point 5 5 nil)) nil)
 (check-expect (distanceSQ nil (point 5 5 nil)) nil)
 (check-expect (distanceSQ (point 0 -5 nil) (point 5 5 nil)) 125)
 
 ;tests for crossProduct
-(check-expect (equal (crossProduct (point 0 0 nil) (point 0 5 nil) (point 1 1 nil))
+(defthm crossProduct-is-Scalar
+   (implies (and (point-p a) (point-p b) (point-p c))
+            (rationalp (crossProduct a b c))))
+(check-expect (equal (crossProduct (point 0 0 nil) 
+                                   (point 0 5 nil) (point 1 1 nil))
                      0) nil)
 (check-expect (crossProduct (point 0 0 nil) (point 0  5 nil) (point 0 1 nil))
               0)
-(check-expect (crossProduct (point nil 0 nil) (point 0 5 nil) (point 1 1 nil)) nil)
+(check-expect (crossProduct (point nil 0 nil) (point 0 5 nil) (point 1 1 nil)) 
+              nil)
 (check-expect (crossProduct nil (point 0 5 nil) (point 1 1 nil)) nil)
                      
 
@@ -59,28 +102,33 @@
 (check-expect (updateLeftFace nil 3 1) nil)
 
 ;tests for findEdge
-(check-expect (findEdge 1 3 (avl-insert (empty-tree) 1 (vEdge 1 3 nil nil)) 1) 1)
-(check-expect (findEdge 5 3 (avl-insert (empty-tree) 1 (vEdge 1 3 nil nil)) 1) nil)
-(check-expect (findEdge 1 3 (avl-insert (empty-tree) 1 (vEdge 1 nil nil nil)) 1) nil)
+(check-expect (findEdge 1 3 (avl-insert (empty-tree) 1 (vEdge 1 3 nil nil))
+                         1) 1)
+(check-expect (findEdge 5 3 (avl-insert (empty-tree) 1 (vEdge 1 3 nil nil))
+                         1) nil)
+(check-expect (findEdge 1 3 (avl-insert (empty-tree) 1 (vEdge 1 nil nil nil))
+                         1) nil)
 (check-expect (findEdge 1 3 (empty-tree) 1) nil)
 
 ;tests for updateEdges
 
 
-;tests for findPoint
-(defun samplePoints ()
-   (avl-insert (avl-insert (avl-insert (avl-insert (empty-tree) 
-                                                   0 (point 1 4 nil))
-                                       1 (point 0 3 nil)) 
-                           2 (point 0 3 nil)) 
-               3 (point 1 2 nil)))              
-(check-expect (findPoint 0 0 2 (samplePoints) 4) 3)
-(check-expect (findPoint 0 1 2 (samplePoints) 4) 5)
-(check-expect (findPoint 0 0 -2 (samplePoints) 4) nil)
-(check-expect (findPoint 0 0 2 (empty-tree) 4) nil)
+;tests for findPoint          
+(check-expect (findPoint 0 0 2 (samplePoints) 10) 4)
+(check-expect (findPoint 0 1 2 (samplePoints) 10) 0)
+(check-expect (findPoint 0 0 -2 (samplePoints) 10) nil)
+(check-expect (findPoint 0 0 2 (empty-tree) 10) nil)
  
 
 ;tests for bestPoint
+(check-expect (bestPoint 0 0 (circle (point 7/2 3/2 nil) 29/2)
+                         1 2 (samplePoints) 10) 5)
+(check-expect (bestPoint 0 0 nil
+                         1 2 (samplePoints) 10) nil)
+(check-expect (bestPoint 0 0 (circle (point 7/2 3/2 nil) 29/2)
+                         321 2 (samplePoints) 10) nil)
+(check-expect (bestPoint 0 0 (circle (point 7/2 3/2 nil) 29/2)
+                         1 2 nil 10) nil)
 
 
 ;tests for completeFacet
@@ -89,31 +137,64 @@
 ;tests for triangulate
 
 
-;tests for closest-help
+;tests for findClosestNeighors+help
+(check-expect (findClosestNeighbors 0 -1 (samplePoints) 10 0 0) (point 1 5 nil))
+(check-expect (findClosestNeighbors 0 -1 (samplePoints) -10 0 0) nil)
+(check-expect (findClosestNeighbors 0 -1 (samplePoints) 10 11 0) nil)
+(check-expect (findClosestNeighbors 0 -1 nil 10 0 0) nil)
 
 
-;tests for findClosestNeighors
-
-
-;tests for conatins
-
+;tests for contains
+(check-expect (contains 5 (list 1 3 5 6)) T)
+(check-expect (contains (list 1 2) (list (list 4) (list 1 2))) T)
+(check-expect (contains nil (list 1 3 6)) nil)
+(check-expect (contains nil nil) nil)
 
 ;tests for prepData
+(check-expect (tree? (prepdata 19 0 (sampleEdges) (empty-tree))) t)
+(check-expect (prepdata 1 0 (avl-insert (avl-insert (empty-tree) 
+                                         0 (vEdge 2 5 0 0)) 
+                                        1 (vEdge 2 3 0 0)) (empty-tree)) 
+              (avl-insert (empty-tree) 2 (list 3 5)))
+(check-expect (prepdata -19 0 (sampleEdges) (empty-tree)) (empty-tree))
+(check-expect (prepdata 19 0 nil (empty-tree)) nil)
+
 
 
 ;tests for sameSide
+(check-expect (sameSide (point 2 2 nil) (point 0 5 nil) 
+                        (point 0 0 nil) (point 5 0 nil)) T)
+(check-expect (sameSide (point -2 -2 nil) (point 0 -5 nil) 
+                        (point 0 0 nil) (point -5 0 nil)) T)
+(check-expect (sameSide (point 2 2 nil) (point nil 5 nil) 
+                        (point 0 0 nil) (point 5 0 nil)) nil)
+(check-expect (sameSide nil (point 0 5 nil) 
+                        (point 0 0 nil) (point 5 0 nil)) nil)
 
 
 ;tests for pointInTriangle
+(check-expect (pointInTriangle 10 (samplePoints) (point 0 0 nil) 
+                               (point 5 0 nil) (point 0 10 nil)) T)
+(check-expect (pointInTriangle 10 (samplePoints) (point 0 0 nil) 
+                               (point 5 0 nil) (point 0 3 nil)) nil)
+(check-expect (pointInTriangle 10 nil (point 0 0 nil) 
+                               (point 5 0 nil) (point 0 3 nil)) nil)
+(check-expect (pointInTriangle 10 (samplePoints) (point 0 0 nil) 
+                               nil (point 0 3 nil)) nil)
 
 
-;tests for process-help
-
-
-;tests for processEdges
-
-
-;tests for genTriangles
-
+;tests for genTriangles+help
+(check-expect (car (genTriangles 10 0 (samplePoints) 
+                                 (prepdata 18 0 (sampleEdges) (empty-tree))
+                                 (empty-tree)))
+              (triangle (point 0 0 NIL) (point -2 -2 NIL) (point 5 0 NIL)))
+(check-expect (len (genTriangles 10 0 (samplePoints) 
+                                 (prepdata 18 0 (sampleEdges) (empty-tree))
+                                 (empty-tree))) 10)
+(check-expect (genTriangles 10 0 nil 
+                                 (prepdata 18 0 (sampleEdges) (empty-tree))
+                                 (empty-tree)) nil)
+(check-expect (genTriangles 10 0 (samplePoints) nil (empty-tree)) nil)
+               
 
 ;tests for delstart
--- Delaunay.lisp
+++ Delaunay.lisp
@@ -11,9 +11,16 @@
 
 
 (defstructure vEdge s tt l r (:options :slot-writers))
-(defstructure circle (c (:assert (point-p c)) )
-   (r2 (:assert (natp r2))) (:options :slot-writers))
-(defstructure tData nPoints nEdges points edges (:options :slot-writers))
+(defstructure circle 
+   (c (:assert (point-p c)))
+   (r2 (:assert (natp r2))) 
+   (:options :slot-writers))
+(defstructure tData 
+   (nPoints (:assert (natp nPoints))) 
+   (nEdges (:assert (natp nEdges))) 
+   points 
+   edges 
+   (:options :slot-writers))
 (defstructure helper minDist u v)
 
 
@@ -51,16 +58,19 @@
 (defun strings->num-lists (lst)
    (if (endp lst)
        nil
-       (cons (parseNums (remove nil (packets-set (list #\, #\' #\space) (coerce (car lst) 'list))))
+       (cons (parseNums (remove nil (packets-set (list #\, #\' #\space)
+                                                  (coerce (car lst) 'list))))
              (strings->num-lists (cdr lst)))))
 
+;distance squared between points a and b
 (defun distanceSQ (a b)
    (if (and (point-p a) (point-p b))
        (let* ((dx (- (point-x a) (point-x b)))
               (dy (- (point-y a) (point-y b))))
-             (+ (* dx dx) (* dy dy)))
+             (abs (+ (* dx dx) (* dy dy)))) ;acl2 will not prove >=0 without abs
        nil))
 
+;2D crossProduct of 3 points which results in a scalar
 (defun crossProduct (p1 p2 p3)
    (if (and (point-p p1) (point-p p2) (point-p p3))
 	   (let* ((u1 (- (point-x p2) (point-x p1)))
@@ -70,6 +80,7 @@
 	         (- (* u1 v2) (* v1 u2)))
        nil))      
 
+;creates or updates a circle where p1-p3 exist on the edge.
 (defun circumCircle (bC p1 p2 p3)
    (if (and (point-p p1) (point-p p2) (point-p p3))
 	   (let* ((cP (crossProduct p1 p2 p3))
@@ -92,6 +103,7 @@
 	             (circle (point cx cy nil) (distanceSQ (point cx cy nil) p1))))
        nil))             
 
+;modifies existing edge using input for orientation and state       
 (defun updateLeftFace (edge s nFaces)
    (if (vEdge-p edge)
        (if (and (equal s (vEdge-s edge))
@@ -103,6 +115,8 @@
                nil))
    nil))
                                  
+;searches edges for an edge with point references s and tt as the end points
+;returns the avl key containing the edge if found.
 (defun findEdge (s tt edges nEdges)
    (if (not (and (posp nEdges) (natp s) (natp tt) (tree? edges)))
        nil
@@ -114,7 +128,8 @@
                 nEdges
                 (findEdge s tt edges (1- nEdges))))))
     
-;returns tData
+;updates triangulation data with new edges and modifies previous 
+;edges if their associated triangles have been determined
 (defun updateEdges (eI s tt bP triData)
    (let* ((edges (tData-edges triData))
           (newEdge1 (updateLeftFace (cdr (avl-retrieve edges eI)) s 0))
@@ -123,16 +138,26 @@
           (nEdges1 (+ (tData-nEdges triData) (if (NULL eI1) 1 0)))
           (edges2 (if (NULL eI1)
                       (avl-insert edges1 (tData-nEdges triData)
-                                  (if (< bP s) (vEdge bP s 1 nil) (vEdge s bP nil 1)))
-                      (avl-insert edges1 eI1 (updateLeftFace (cdr (avl-retrieve edges1 eI1)) bP 1))))
+                                  (if (< bP s) 
+                                      (vEdge bP s 1 nil) 
+                                      (vEdge s bP nil 1)))
+                      (avl-insert edges1 eI1 (updateLeftFace 
+                                              (cdr (avl-retrieve edges1 eI1))
+                                                             bP 1))))
           (eI2 (findEdge tt bP edges2 nEdges1))
           (nEdges2 (+ nEdges1 (if (NULL eI2) 1 0)))
           (edges3 (if (NULL eI2)
                       (avl-insert edges2 nEdges1 
-                                  (if (< tt bP) (vEdge tt bP 1 nil) (vEdge bP tt nil 1)))
-                      (avl-insert edges2 eI2 (updateLeftFace (cdr (avl-retrieve edges2 eI2)) tt 1)))))
+                                  (if (< tt bP) 
+                                      (vEdge tt bP 1 nil) 
+                                      (vEdge bP tt nil 1)))
+                      (avl-insert edges2 eI2 (updateLeftFace 
+                                              (cdr (avl-retrieve edges2 eI2)) 
+                                                             tt 1)))))
          (set-tData-edges edges3 (set-tData-nEdges nEdges2 triData))))
 
+;quick serach to find the first usable point before using the 
+;more expensive bestPoint function to find the final point
 (defun findPoint (u s tt points nPoints)
    (if (and (natp s) (natp tt) (tree? points) (natp nPoints))
        (if (or (zp (- nPoints u)) (> u nPoints))
@@ -149,21 +174,27 @@
                              (findPoint (1+ u) s tt points nPoints))))))
        nil))
        
+;checks the remaining points to find the only point meeting the 
+;criteria for a delaunay triangle       
 (defun bestPoint (u bP bC s tt points nPoints)
-   (if (or (zp (- u nPoints)) (> u nPoints))
-       bP
-       (if (or (= u s) (= u tt))
-           (bestPoint (1+ u) bP bC s tt points nPoints)
-           (let* ((pS (cdr (avl-retrieve points s)))
-                  (pT (cdr (avl-retrieve points tt)))
-                  (pU (cdr (avl-retrieve points u))))
-                 (if (and (> (crossProduct pS pT pU) 0)
-                          (< (distanceSQ (circle-c bC) pU) 
-                             (circle-r2 bC)))
-                     (bestPoint (1+ u) u (circumCircle bC pS pT pU) s tt points nPoints)
-                     (bestPoint (1+ u) bP bC s tt points nPoints))))))
+   (if (or (NULL bC) (> bP nPoints) (> s nPoints) (> tt nPoints) (NULL points))
+       nil
+	  (if (or (zp (- nPoints u)) (> u nPoints))
+	      bP
+	      (if (or (= u s) (= u tt))
+	          (bestPoint (1+ u) bP bC s tt points nPoints)
+	          (let* ((pS (cdr (avl-retrieve points s)))
+	                 (pT (cdr (avl-retrieve points tt)))
+	                 (pU (cdr (avl-retrieve points u))))
+	                (if (and (> (crossProduct pS pT pU) 0)
+	                         (< (distanceSQ (circle-c bC) pU) 
+	                            (circle-r2 bC)))
+	                    (bestPoint (1+ u) u (circumCircle bC pS pT pU) 
+                                s tt points nPoints)
+	                    (bestPoint (1+ u) bP bC s tt points nPoints)))))))
 
-;returns tData
+;Uses the key to current edge(eI) and current triangulation data to find a 
+;point to create a new triangle using the current edge as a base
 (defun completeFacet (eI triData)
    (let* ((edges (tData-edges triData))
           (edge (cdr (avl-retrieve edges eI)))
@@ -178,24 +209,37 @@
                                 (cdr (avl-retrieve points tt))
                                 (cdr (avl-retrieve points bP)))
                   nil))
-          (bP2 (if (< bP nPoints) (bestPoint bP bP bC s tt points nPoints) bP)))
+          (bP2 (if (< bP nPoints) 
+                   (bestPoint bP bP bC s tt points nPoints) 
+                   bP)))
          (if (> bP2 nPoints)
-             (set-tData-edges (avl-insert edges eI (updateLeftFace edge s 0)) triData)
+             (set-tData-edges (avl-insert edges eI (updateLeftFace edge s 0)) 
+                              triData)
              (updateEdges eI s tt bP2 triData))))
      
 ;main recursive call
+;cycles until no new edges are created
+;creates edges l and r unless they already exist, which create a triangle 
+;using the current edge. 
+;l and r are added to edges being processed once created    
 (defun triangulate (currentEdge triData)
    (if (or (zp (- currentEdge (tData-nEdges triData)))
            (> currentEdge (tData-nEdges triData)))
        triData
-       (let* ((l (vEdge-l (cdr (avl-retrieve (tData-edges triData) currentEdge))))
-              (triData1 (if (NULL l) (completeFacet currentEdge triData) triData))
-              (r (vEdge-r (cdr (avl-retrieve (tData-edges triData1) currentEdge))))                
-              (triData2 (if (NULL r) (completeFacet currentEdge triData1) triData1)))
+       (let* ((l (vEdge-l (cdr (avl-retrieve (tData-edges triData) 
+                                             currentEdge))))
+              (triData1 (if (NULL l) (completeFacet currentEdge triData) 
+                            triData))
+              (r (vEdge-r (cdr (avl-retrieve (tData-edges triData1) 
+                                             currentEdge))))                
+              (triData2 (if (NULL r) (completeFacet currentEdge triData1) 
+                            triData1)))
              (triangulate (1+ currentEdge) triData2)))) 
 
+;helper function for findClosestNeighbors which checks the distance all 
+;points(j) in relation to i, updates u v as keys to closest pair found.
 (defun closest-help (i j minDist points nPoints u v)
-   (if (or (zp (- j nPoints))
+   (if (or (zp (- nPoints j))
            (> j nPoints))
        (helper minDist u v)
        (let ((d (distanceSQ (cdr (avl-retrieve points i))
@@ -205,15 +249,20 @@
                 (closest-help i (1+ j) d points nPoints i j)
                 (closest-help i (1+ j) minDist points nPoints u v)))))
 
+;searches all points and returns the avl key to the two closest points
 (defun findClosestNeighbors (i minDist points nPoints u v)
-   (if (or (zp (- i (1- nPoints)))
-           (> i (1- nPoints)))
-       (if (< u v) (point u v nil) (point v u nil))
-       (let ((ret (closest-help i (1+ i) minDist points nPoints u v)))
-            (findClosestNeighbors (1+ i) (helper-minDist ret) 
-                                  points nPoints
-                                  (helper-u ret) (helper-v ret)))))
-   
+   (if (or (> u nPoints) (> v nPoints) (NULL points) (not (natp nPoints)))
+       nil
+	  (if (or (zp (- (1- nPoints) i))
+	          (> i (1- nPoints)))
+	      (if (< u v) (point u v nil) (point v u nil))
+	      (let ((ret (closest-help i (1+ i) minDist points nPoints u v)))
+	           (findClosestNeighbors (1+ i) (helper-minDist ret) 
+	                                 points nPoints
+	                                 (helper-u ret) (helper-v ret))))))
+
+;converts xs (list of list of numbers) into ys (avl-tree of points)
+;updates n and merged with result to indicate number of used points   
 (defun parse (n xs ys)
    (if (endp xs)
        (cons n ys)
@@ -221,41 +270,51 @@
             (parse (1+ n) (cdr xs) (avl-insert ys n p)))))
  
 (defun contains (x xs)
-   (if (endp xs)
+   (if (or (endp xs) (NULL x))
        nil
-       (if (= x (car xs))
+       (if (equal x (car xs))
            t
            (contains x (cdr xs)))))
 
 ;convert triangulation results into sorted set of 
 ;edge references for triangle extraction
 (defun prepdata (nEdges n edges tree)
-   (if (< nEdges 0)
-       tree
-       (let* ((edge (cdr (avl-retrieve edges nEdges)))
-              (key (vEdge-s edge))
-              (data (cdr (avl-retrieve tree key))))
-             (prepdata (1- nEdges) (max n key) edges
-                        (avl-insert tree key (append data (list (vEdge-tt edge))))))))
+   (if (and (tree? edges) (not (NULL edges)))
+	   (if (< nEdges 0)
+	       tree
+	       (let* ((edge (cdr (avl-retrieve edges nEdges)))
+	              (key (vEdge-s edge))
+	              (data (cdr (avl-retrieve tree key))))
+	             (prepdata (1- nEdges) (max n key) edges
+	                        (avl-insert tree key 
+                                     (append data (list (vEdge-tt edge)))))))
+       nil))
 
 (defun sameSide (p1 p2 a b)
-   (let* ((cp1 (crossProduct a b p1))
-          (cp2 (crossProduct a b p2)))
-         (>= (* cp1 cp2) 0)))
+   (if (and (point-p p1) (point-p p2) (point-p a) (point-p b))
+	  (let* ((cp1 (crossProduct a b p1))
+	         (cp2 (crossProduct a b p2)))
+	        (>= (* cp1 cp2) 0))
+       nil))
 
+;checks all points and returns true if any point exists within triangle(a,b,c)
 (defun pointInTriangle (n points a b c)
-   (if (< n 0)
-       nil
-       (let ((p (cdr (avl-retrieve points n))))
-            (if (or (equal p a) (equal p b) (equal p c))
-                (pointInTriangle (1- n) points a b c)
-                (if (and (sameSide p a b c)
-                         (sameSide p b a c)
-                         (sameSide p c a b))
-                    T
-                    (pointInTriangle (1- n) points a b c))))))
+   (if (and (natp n) (not (NULL points)) (point-p a) (point-p b) (point-p c))
+	   (if (< n 0)
+	       nil
+	       (let ((p (cdr (avl-retrieve points n))))
+	            (if (or (equal p a) (equal p b) (equal p c))
+	                (pointInTriangle (1- n) points a b c)
+	                (if (and (sameSide p a b c)
+	                         (sameSide p b a c)
+	                         (sameSide p c a b))
+	                    T
+	                    (pointInTriangle (1- n) points a b c)))))
+       nil))
 
-;had to split up process functions to prove termination
+;cycles through points connected with the second point
+;performs final confirmation of connectivity between all three points
+;and adds to list of triangles if the test passes.
 (defun process-help (nPoints n n2 points tree tris set)
    (if (endp set)
        tris
@@ -276,48 +335,51 @@
                              (append tris (list tri))))))
              (process-help nPoints n n2 points tree tris2 (cdr set)))))
 
+
+;cycles through points connected with the first point and forwards points 
+;to helper to determine if a third point exist to create a valid triangle
 (defun processEdges (nPoints n points tree tris set)
    (if (endp (cdr set))
        tris
        (processEdges nPoints n points tree 
-                     (process-help nPoints n (car set) points tree tris (cdr set))
+                     (process-help nPoints n (car set) 
+                                   points tree tris (cdr set))
                      (cdr set))))
 
+
+;cycles through new edge connectivity tree and sends first point to 
+;process edges to find true Delaunay triangles
 (defun genTriangles (nPoints n points tree tris)
-   (let ((set (cdr (avl-retrieve tree n))))
-        (if (or (zp nPoints) nil);(NULL set))
-            tris
-            (genTriangles (1- nPoints) (1+ n) points tree (processEdges nPoints n points tree tris set)))))         
+   (if (or (NULL points) (NULL tree))
+       nil
+	  (let ((set (cdr (avl-retrieve tree n))))
+	       (if (or (zp nPoints) nil);(NULL set))
+	           tris
+	           (genTriangles (1- nPoints) (1+ n) points tree (processEdges nPoints n points tree tris set))))))
    
 ;starter function
+;Reads list of list of numbers and returns list of triangles
 (defun delstart (xs)
    (let* ((parsed (parse 0 xs (empty-tree)))
           (pair (findCLosestNeighbors 0 -1 (cdr parsed) (car parsed) 0 0))
           (edge (vEdge (point-x pair) (point-y pair) nil nil))
-          (data (tData (car parsed) 1 (cdr parsed) (avl-insert (empty-tree) 0 edge)))
+          (data (tData (car parsed) 1 (cdr parsed) 
+                       (avl-insert (empty-tree) 0 edge)))
           (triData (triangulate 0 data))
-          (triangles (genTriangles (tData-nPoints triData) 0 (tData-points triData)
-                                   (prepdata (1- (tData-nEdges triData)) 0 (tData-edges triData) (empty-tree)) nil)))
-         triangles)) ;replace with call to svg generator
+          (triangles (genTriangles (tData-nPoints triData) 0 
+                                   (tData-points triData)
+                                   (prepdata (1- (tData-nEdges triData))
+                                              0 (tData-edges triData) 
+                                             (empty-tree))
+                                   nil)))
+         triangles))
     
 
 (defun svgLines (triangulation i)
    (if (consp triangulation)
        (cons (svgTriangle (car triangulation) i)
              (svgLines (cdr triangulation) (1+ i)))
-       nil))                         
-                                                                   
-;quick test code based on expected live input format (list of (lat, lon, color))
-#| Removed as ACL2 will not accept this...                                                                    
-(let* ((input (list "34.81, -98.02, '15,0,255'"
-                    "34.80, -96.67, '31,0,255'"
-                    "34.59, -96.67, '0,63,255'"
-                    "35.40, -99.90, '0,15,255'"
-                    "34.04, -96.35, '63,0,255'"
-                    "36.63, -96.81, '47,0,255'"))
-       (points (strings->num-lists input)))
-      (svgLines  (delstart points) 0))
-|#
+       nil))                              
                                                                    
 (defun string-list->stdout (strli state)
   (let ((channel *standard-co*))
@@ -329,8 +391,6 @@
             (let ((state (close-output-channel channel state)))
               (mv nil state))))))
 
-
-
 (defun main (state)
   (mv-let (input-lines state)
           (stdin->lines state)
@@ -338,12 +398,15 @@
          (mv nil state)
          (mv-let (error-close state)
                  (string-list->stdout ;input-lines
-                  (append
-                                     (cons "<g transform=\"translate(8.6472902 5.1103759) scale(111.5435 135.7388) translate(103 37) scale(1 -1)\">"
-                                     
-                                      (svgLines (delstart (strings->num-lists input-lines)) 0)
-                                     )(list "</g>"))
-                                       state)
+                 	(append
+                    	(cons "<g transform=\"translate(8.6472902 5.1103759) 
+                        		scale(111.5435 135.7388) 
+                         	translate(103 37) scale(1 -1)\">"
+                         	(svgLines 
+                          		(delstart (strings->num-lists input-lines)) 
+                                   0)
+                         )(list "</g>"))
+                  	state)
             (if error-close
                 (mv error-close state)
                 (mv (string-append "input file: "
