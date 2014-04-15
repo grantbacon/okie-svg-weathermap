7dcafebca7bfc3ea977b6136aa02f2a495de67e1
Joel Maupin <jmaupin82@gmail.com>
2014-04-15 00:40:55 -0500

    committed some tests.

--- svg-tst.lisp
+++ svg-tst.lisp
@@ -7,6 +7,21 @@
 (check-expect (semicolon) #\;)
 (check-expect t (stringp (semicolon)))
 
+;oppositePoint Tests
+(check-expect (oppositePoint (point 0 5 nil) (point -3 1 nil) 
+                             (point 3 1 nil)) (point 0 1 nil))
+(check-expect (oppositePoint (point 3 4 nil) (point -3 1 nil)
+                              (point 3 1 nil)) (point 3 1 nil))
+(check-expect (oppositePoint (oppositePoint (point 0 3 nil) 
+                              (point -3 -1 nil) (point 0 0 nil)) (POINT 9/10 3/10 NIL))
+(check-expect (oppositePoint (point 0 3 nil) (point -3 -1 nil)
+                              (point 3 6 nil)) (POINT 21/85 237/85 NIL))
+
+; color->str tests
+(check-expect "255,255,255" (color->str '(255 255 255)))
+(check-expect "0,0,0" (color->str '(0 0 0) ))
+(check-expect "255,134,0" (color->str '(255 134 0) ))
+(check-expect "0,23,54" (color->str '(0 23 54) ))
 (defproperty color->str-returns-a-string
    (c1 :value (random-integer)
     c2 :value (random-integer)
@@ -22,4 +37,3 @@
    (let* ((p1 (point x1 y1 nil))
       	(p2 (point x2 y2 nil)))
         (equal t (stringp (svgGradient p1 p2 "a" "233,444,222")))))
-
